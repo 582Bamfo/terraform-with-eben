7b9481e3bc94489389c3899f4c269e370858a64e
Ebenezer <nan_santi@yahoo.com>
2026-03-05 12:09:45 +0000

    added instance metadata options and updated IAM destroy options

--- main.tf
+++ main.tf
@@ -9,20 +9,26 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-1"
+   region = "eu-west-1"
 }
 
 resource "aws_instance" "this" {
   ami           = "ami-03446a3af42c5e74e"
   instance_type = "t2.micro"
+
+  metadata_options {
+       http_endpoint          = "enabled"
+       http_token             = "required"
+       instance_metadata_tags = "enabled"
+  }
   tags = {
-    Name = "web"
+     Name = "web"
   }
 }
 
 resource "aws_iam_user" {
   name = "vcristian"
-
+  force_destroy = false
   tags = {
     Environment = "dev"
   }
