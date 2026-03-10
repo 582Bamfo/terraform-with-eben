caa2580ef27a8c1ba75aa2b2b737a43d1b39039a
Ebenezer <nan_santi@yahoo.com>
2026-03-10 11:12:08 +0000

    refactor the code into modules and variables

---
+++ iam_group/variables.tf
@@ -0,0 +1,15 @@
+variable "group_name" {
+  description = "Name of the IAM group"
+  type        = string
+}
+
+variable "policy_arn" {
+  description = "Policy to attach to the group"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for IAM group"
+  type        = map(string)
+  default     = {}
+}
--- main.tf
+++ main.tf
@@ -3,15 +3,6 @@ module "instance" {
   instance_type = "t2.small"
 }
 
-resource "aws_iam_group" "dev" {
-  name = "dev"
-}
-
-resource "aws_iam_group_policy_attachment" "dev_policy" {
-  group      = aws_iam_group.dev.name
-  policy_arn = var.policy_arn
-}
-
 resource "aws_iam_user_group_membership" "dev_membership" {
   user   = aws_iam_user.user.name
   groups = [aws_iam_group.dev.name]
