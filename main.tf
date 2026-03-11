79010357d574fadd0e72ba572c0cb2344e5c8c66
Ebenezer <nan_santi@yahoo.com>
2026-03-11 12:49:45 +0000

    fixed  main.tf with undeclared resource

--- main.tf
+++ main.tf
@@ -3,7 +3,4 @@ module "instance" {
   instance_type = "t2.small"
 }
 
-resource "aws_iam_user_group_membership" "dev_membership" {
-  user   = aws_iam_user.user.name
-  groups = [aws_iam_group.dev.name]
-}
+
