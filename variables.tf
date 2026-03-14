7960d4a822956b7fe8aeeb3b0aca5e4a46616963
dbamfo <bamfodaniel@yahoo.com>
2026-03-14 21:53:32 +0000

    modified env

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "tags" {
   default = {
-    Environment = "dev"
+    Environment = "prod"
     managed_by  = "terraform"
   }
   description = "tags for the resources"
