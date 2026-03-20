3628a60af1484477d743f955a2e85bcc94c0bf03
cloudeng-tetteh <kwabsante@yahoo.com>
2026-03-20 14:18:58 +0000

    added vpc and created corresponding variables

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "instance" {
 
 module "iam_user" {
   source = "./iam_user"
-  iam_user_name = var.iam_user_name
+
 }
 
 module "s3" {
@@ -16,4 +16,20 @@ module "iam_group" {
   source     = "./iam_group"
   policy_arn = var.policy_arn
 
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
 }
